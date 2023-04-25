ee356dfcf56de68caa620fc3b0e6a5e93a900fbb
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-04-25 16:10:09 -0300

    aula 3

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const consultaCEP = fetch("https://viacep.com.br/ws/0101000/json/")
+  .then((resposta) => resposta.json())
+  .then((r) => {
+    if (r.erro) {
+      throw Error("Esse CEP não existe!");
+    } else {
+      console.log(r);
+    }
+  })
+  .catch((erro) => console.log(erro))
+  .finally((mensagem) => console.log("Processamento concluido"));
