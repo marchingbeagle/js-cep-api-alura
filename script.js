3fe3bdc87c0f3405539e46980671f4887ada8165
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-04-26 09:28:34 -0300

    autopreenchimento de cep aplicado + tratamento de erros na tela

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-const consultaCEP = fetch("https://viacep.com.br/ws/0101000/json/")
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error("Esse CEP não existe!");
-    } else {
-      console.log(r);
+const inputCEP = document.querySelector("#cep");
+
+inputCEP.addEventListener("focusout", () => {
+  buscaCEP(inputCEP.value);
+});
+
+async function buscaCEP(cep) {
+  try {
+    const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("Cep não existente");
     }
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log("Processamento concluido"));
+    autoPreenchimentoEndereco(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    trataErro();
+  }
+}
+
+function autoPreenchimentoEndereco(consultaCEPConvertida) {
+  const cidade = document.querySelector("#cidade");
+  const estado = document.querySelector("#estado");
+  const logradouro = document.querySelector("#endereco");
+  const bairro = document.querySelector("#bairro");
+  document.querySelector(".erro").innerHTML = "";
+
+  cidade.value = consultaCEPConvertida.localidade;
+  estado.value = consultaCEPConvertida.uf;
+  logradouro.value = consultaCEPConvertida.logradouro;
+  bairro.value = consultaCEPConvertida.bairro;
+}
+
+function trataErro() {
+  const divErro = document.createElement("div");
+  divErro.classList.add("erro");
+  document.querySelector("#cep").parentElement.appendChild(divErro);
+  document.querySelector(".erro").innerHTML =
+    "<p> CEP inválido, tente novamente! </p>";
+}
